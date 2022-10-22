4ab5bf1d5449002fcbe3fc9b96505a0ec7b857db
1274738894 <1274738894@qq.com>
2022-10-22 14:28:12 +0800

    webpack多入口

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import Str from './str'
+let a='<h3>33333</h3>'
+const app=document.getElementById('app')
+app.innerHTML=a+Str.link;
---
+++ src/one.js
@@ -0,0 +1,3 @@
+import Two from "./two";
+let x=200;
+console.log(x+Two.y);
